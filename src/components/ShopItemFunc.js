f55ab8bbbd93622aaa4ba441500cfacc2724128f
Sergey <84701382+Sergl82@users.noreply.github.com>
2022-06-06 22:40:29 +1100

    ra-components-2

---
+++ src/components/ShopItemFunc.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Item from './Item';
+
+class ShopItemFunc extends React.Component {
+   render() {
+      const { item } = this.props;
+      return (
+       <div class="main-content">
+         <h2>{item.brand}</h2>
+         <h1>{item.title}</h1>
+         <h3>{item.description}</h3>
+         <div class="description">{item.descriptionFull}</div>
+         <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+         <div class="divider"></div>
+         <div class="purchase-info">
+           <div class="price">{item.currency}{item.price.toFixed(2)}</div>
+           <button>Добавить в корзину</button>
+         </div>
+       </div>
+      );
+   }
+}
+
+ShopItemFunc.protoType = {
+   item: PropTypes.instanceOf(Item).isRequired
+}
+
+export default ShopItemFunc;
